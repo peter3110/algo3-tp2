3b3652131742cdcb877dd91e6e7b862cb5a039e6
Pedro R <pedrorodriguezsjs@hotmail.com>
2014-09-11 01:39:32 -0300

    modifico el prob_vuelos.cpp para que ande, pero sin la optimizacion de usar bitmasks.. faltaria eso para que dé la complejidad

--- problema1/prob_vuelos.cpp
+++ problema1/prob_vuelos.cpp
@@ -13,61 +13,77 @@ struct vuelo {
 
 int main() {
 	
+	freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+    
     int n, inicio, fin;
+    int horarioLlegada, ultimo;
     string A, B, origen, destino;
-    cin >> A >> B >> n;
-    
     vector<vuelo> vuelos(n);
-    vector<int> combinaciones(n);        // combinaciones[i] = el conjunto de los vuelos desde los cuales se puede acceder al vuelo i
-    int puedoLlegar;                     // conjunto de vuelos a los que puedo llegar si como origen estoy en A
+    vector<int> res, puedoLlegar;
+    vector< vector<int> > combinaciones;   
     
-    for(int i=0; i<n; i++) {
-        cin >> origen >> destino >> inicio >> fin;
-        vuelos[i].origen = origen; vuelos[i].destino = destino;
-        vuelos[i].inicio = inicio; vuelos[i].fin = fin;
-    }
-    
-    for(int i=0; i<n; i++) {                                            // al principio, el vector combinaciones tiene todos los vuelos que se pueden conectar de a pares
-        for(int j=0; j<n; j++) {                                        // y después, en base a esta inicialización, voy actualizando hasta considerar todos los vuelos
-            if(vuelos[i].destino == vuelos[j].origen && 
-               vuelos[i].fin + 2 <= vuelos[j].inicio) {     // se puede llegar de i a j
-                    combinaciones[j] += (1<<i);
-                    if(vuelos[i].origen == A) {puedoLlegar += (1<<i); } // el vuelo i tiene origen en A. Despues voy a actualizar este entero (que representa un cjto.) y
-                                                                        // va a terminar diciendome a qué vuelos puedo llegar desde A.
-            }
-        }
-    }
-    
-    /* Aca se hacen todas las comparaciones necesarias para saber, para cada viaje, si puede ser accedido
-     * desde algún otro viaje al cuál se pueda acceder desde uno que tenga como origen a A */
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-                
-                if((combinaciones[i] & (1<<j)) != 0) {                  // puedo llegar al vuelo i desde el vuelo j
-                    combinaciones[i] += combinaciones[j];               // actualizo desde dónde puedo llegar al vuelo i
-                    if((puedoLlegar & (1<<j)) != 0) {
-                            puedoLlegar += (1<<i) + combinaciones[i];
-                    }                                                   // también puedo llegar al vuelo i  y a los que se puede llegar desde i, desde A
-                }
-                
-                if((combinaciones[j] & (1<<i)) != 0) {                  // puedo llegar al vuelo j desde el vuelo i
-                    combinaciones[j] += combinaciones[i];               // actualizo desde dónde puedo llegar al vuelo j
-                    if((puedoLlegar & (1<<i)) != 0) {
-                            puedoLlegar += (1<<j) + combinaciones[j];   // también puedo llegar al vuelo j y los que se puede llegar desde j, desde A
-                    } 
-                }
-        }
-    }
-    
-    /* Ahora que ya tengo completo el vector "combinaciones" y el entero puedoLlegar, ya sé para cada viaje si se puede acceder 
-     * desde el origen A. Entonces, me fijo cuál es el vuelo "i" que llega primero al destino B, de los vuelos a los cuáles se puede
-     * acceder desde el origen A. Así, voy yendo para atrás, cada vez a algún vuelo cuyo destino sea el origen del vuelo i,
-     * hasta encontrar un vuelo cuyo origen sea A. */
-    for(int i=0; i<n; i++) {
-        
-    }
-    
-    
-    
-    return 0;
+    while(cin >> A >> B >> n) {
+		
+		/* Inicializo las variables correctamente */
+		ultimo = 0;
+		vuelos.resize(n); 
+		res.resize(0);
+		puedoLlegar.resize(n); fill(puedoLlegar.begin(), puedoLlegar.end(), 0);
+		combinaciones.resize(n); 
+		for(int i=0; i<n; i++) {
+			combinaciones[i].resize(n); 
+			fill(combinaciones[i].begin(), combinaciones[i].end(), 0);
+		}
+		
+		/* Leo los vuelos que tengo y inicializo "combinaciones" y "puedoLlegar" */
+		for(int i=0; i<n; i++) {
+			cin >> origen >> destino >> inicio >> fin;
+			vuelos[i].origen = origen; vuelos[i].destino = destino;
+            vuelos[i].inicio = inicio; vuelos[i].fin = fin;
+            combinaciones[i][i] = 1;
+            if(origen == A) {puedoLlegar[i] = 1; }
+		}
+		
+		/* Relleno "combinaciones" y "puedoLlegar" dinámicamente para todos los pares de vuelos */
+		for(int i=0; i<n; i++) {
+			for(int j=i; j<n; j++) {
+				if(vuelos[i].destino == vuelos[j].origen && 
+                   vuelos[i].fin + 2 <= vuelos[j].inicio) {				// camino directo i->j
+					   combinaciones[i][j] = 1;
+					   if(puedoLlegar[i]) {puedoLlegar[j] = 1;}
+				}
+				if(vuelos[j].destino == vuelos[i].origen && 
+                   vuelos[j].fin + 2 <= vuelos[i].inicio) {				// camino directo j->i
+					   combinaciones[j][i] = 1;
+					   if(puedoLlegar[j]) {puedoLlegar[i] = 1;}
+				}
+			}
+		}
+		
+		/* Relleno "combinaciones" y "puedoLlegar" dinámicamente de forma completa */
+		
+		for(int i=0; i<n;i++) {
+			for(int j=0; j<n; j++) {
+				if(combinaciones[j][i] == 1) {
+					for(int k=0; k<n; k++) {
+						combinaciones[k][i] = max(combinaciones[k][i], combinaciones[k][j]);
+					}
+					if(puedoLlegar[j] == 1) {puedoLlegar[i] = 1;}
+				}
+				// si combinaciones[j][i] == 1 ---> actualizar combinaciones[1..n][i] sumando las de j 
+				// 									si puedoLlegar[j] == 1 ----> puedoLlegar[i] = 1;
+				// (estoy en el caso en que existe camino de j -> i)
+			}
+		}
+		
+		/* Devolver resultado */
+		horarioLlegada = 1000;
+		for(int i=0; i<n; i++) {
+			if((puedoLlegar[i]==1) && (vuelos[i].destino==B)){horarioLlegada = min(horarioLlegada, vuelos[i].fin);}}
+		if(horarioLlegada==1000){cerr<<"no"<<endl;}else{cerr << horarioLlegada << endl;}
+				
+	}
+	
+	return 0;   
 }
