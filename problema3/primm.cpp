133d1ad7b9eba016d5b6b40f070c82cd7558fc5b
Pedro R <pedrorodriguezsjs@hotmail.com>
2014-10-24 14:35:35 -0300

    codigo andano

---
+++ problema3/primm.cpp
@@ -0,0 +1,82 @@
+
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <algorithm>
+#define INT_MAX 10000001
+using namespace std;
+
+bool ordenarPares(pair< pair<int,int> , int > a, pair< pair<int,int>,int > b){
+        return a.second<b.second;
+}
+
+
+int minKey(vector<int>& key, vector<bool>& mstSet, int V)
+{
+   // Initialize min value
+   int min = INT_MAX, min_index;
+
+   for (int v = 1; v <= V; v++)
+     if (mstSet[v] == false && key[v] < min)
+         min = key[v], min_index = v;
+
+   return min_index;
+}
+
+vector<pair < pair<int,int>,int> >  primm(vector<vector<int> >& graph, int cantNodos){
+
+     vector<int> parent(cantNodos+1); // Array to store constructed MST
+     vector<int> key(cantNodos+1);    // Key values used to pick minimum weight edge in cut
+     vector<bool> mstSet(cantNodos+1);  // To represent set of vertices not yet included in MST
+     pair<pair<int, int>,int > temp;
+     vector<pair < pair<int,int>,int> > aristas;
+     // Initialize all keys as INFINITE
+     for (int i = 1; i <= cantNodos; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+
+     // Always include first 1st vertex in MST.
+     key[1] = 1;     // Make key 0 so that this vertex is picked as first vertex
+     parent[1] = -1; // First node is always root of MST
+
+     // The MST will have V vertices
+     for (int count = 1; count <= cantNodos; count++)
+     {
+        // Pick thd minimum key vertex from the set of vertices
+        // not yet included in MST
+        int u = minKey(key, mstSet, cantNodos);
+
+        // Add the picked vertex to the MST Set
+        mstSet[u] = true;
+
+        // Update key value and parent index of the adjacent vertices of
+        // the picked vertex. Consider only those vertices which are not yet
+        // included in MST
+        for (int v = 1; v <= cantNodos; v++){
+
+           // graph[u][v] is non zero only for adjacent vertices of m
+           // mstSet[v] is false for vertices not yet included in MST
+           // Update the key only if graph[u][v] is smaller than key[v]
+          if (mstSet[v] == false && graph[u][v] <  key[v]){
+             parent[v]  = u, key[v] = graph[u][v];
+
+          }
+     }
+     }
+
+     for(int i=2; i<=cantNodos; i++){
+        temp.second=key[i];
+        (temp.first).first=parent[i];
+        (temp.first).second=i;
+        aristas.push_back(temp);
+     }
+
+      // print the constructed MST
+      //cout<<(aristas.top()).first<<endl;
+      //printf("Edge   Weight\n");
+      //for (int i = 1; i < cantNodos; i++){
+      //printf("%d - %d    %f \n", parent[i]+1, i+1, graph[i][parent[i]]);}
+      return aristas;
+}
--- problema3/prob3.cpp
+++ problema3/prob3.cpp
@@ -6,13 +6,15 @@
 #include <math.h>
 #include <assert.h>
 #include <algorithm>
+#include "primm.cpp"
+#define MAX_INT 10000001
 using namespace std;
 
 
 /* Declaro estructuras auxiliares */
 struct nodo{
 	int valor;
-	vector<int> adyacentes;
+	vector<pair<int,int> > adyacentes;		// adyacentes.second tiene la longitud del eje que une nodo.valor con adyacentes.first
 };
 
 /* Declaro las funciones auxiliares */
@@ -21,15 +23,14 @@ bool cmpEquipos(pair< pair<int, int>, int > A, pair< pair<int, int>, int > B);
 void BuscarEjeParaCicloYAgregar(vector< pair< pair<int, int>, int > > &G,
                                 vector< pair< pair<int, int>, int > > &T);
 vector<nodo> BuscarCiclo(vector< pair< pair<int, int>, int > > &T, int n);
-vector<nodo> BuscarResto(vector< pair< pair<int, int>, int > > &T, vector<nodo> &C);
-void DevolverResultado(vector< pair< pair<int, int>, int > > &T,
-                       vector< pair< pair<int, int>, int > > &C,
-                       vector< pair< pair<int, int>, int > > &R);
-vector< pair< pair<int, int>, int > > MST(vector< pair< pair<int, int>, int > > &G);
+void DevolverResultado(pair< vector< pair< pair<int, int>, int > >, vector< pair< pair<int, int>, int > > > &ANS);
 bool Chequear_Conexo(vector< pair< pair<int, int>, int > > &G, int n);
 vector<nodo> generar_grafo_nodos(vector< pair< pair<int, int>, int > > &G, int n);  
 void dfs(nodo base, vector<int> &visitados, vector<nodo> &G2);                
-nodo dfsCiclo(nodo base, vector<int> &visitados, vector<nodo> &G2, vector<int> &padres)     ;                              
+nodo dfsCiclo(nodo base, vector<int> &visitados, vector<nodo> &G2, vector<int> &padres);       
+pair< vector< pair< pair<int, int>, int > >, vector< pair< pair<int, int>, int > > > BuscarCicloYResto(vector< pair< pair<int, int>, int > > &T, int n);                      
+vector< vector<int> > matrizDeAdyacencia(vector< pair< pair<int, int>, int > > &G, int n);
+// vector< pair< pair<int, int>, int > > MST(vector< pair< pair<int, int>, int > > &G, int n);
 
 /*========== Resolvedor del problema ==========*/
 int main() {
@@ -52,30 +53,29 @@ int main() {
   
   /* Armo el MST a partir del grafo G */
   
-  T = MST(G);
+  vector< vector<int> > G2 = matrizDeAdyacencia(G,n);
+  T = primm(G2,n);
+  
+  
+  //~ T = G;
+  //~ G.push_back(make_pair(make_pair(2,3),5));
+  //~ G.push_back(make_pair(make_pair(4,8),10));
+  //~ G.push_back(make_pair(make_pair(2,7),2));
   
   /* Ordeno los ejes de G  y de T de menor a mayor costo, para luego localizar 
    * el menor eje de G que no esta en T y agregarlo a T para formar un ciclo */
    
-  //~ sort(G.begin(), G.end(), cmpEquipos);
-  //~ sort(T.begin(), T.end(), cmpEquipos);
+  sort(G.begin(), G.end(), cmpEquipos);
+  sort(T.begin(), T.end(), cmpEquipos);
   
   vector< pair< pair<int, int>, int > > T2 = T;
   BuscarEjeParaCicloYAgregar(G,T2);		// G y T tienen que estar indexado entre 1..n; tam(T2) = n+2
 										
-  /* Recorrer arbol con un ciclo buscando el ciclo  y devolver dicho ciclo */
-  
-  vector<nodo> C = BuscarCiclo(T2,n);
-  vector<nodo> R = BuscarResto(T2,C);
-  
-  /* Testing de funcion BuscarCiclo */
-  //~ vector<nodo> C = BuscarCiclo(G,n);
-  //~ cout << "El ciclo esta conformado por los nodos: ";
-  //~ for(int i=0; i<(int)C.size(); i++) {cout << C[i].valor << " ";}cout << endl;
+  pair< vector< pair< pair<int, int>, int > >, vector< pair< pair<int, int>, int > > > ANS = BuscarCicloYResto(T2,n);
   
   /* Devolver resultado */
   
-  //DevolverResultado(T,C,R);
+  DevolverResultado(ANS);
   
   return 0;
 }
@@ -98,17 +98,19 @@ bool cmpEquipos(pair< pair<int, int>, int > A, pair< pair<int, int>, int > B) {
   return (A.second < B.second);
 }
 
-vector< pair< pair<int, int>, int > > MST(vector< pair< pair<int, int>, int > > &G) {
-  
-  //~ for(int i=0; i<(int)G.size(); i++) {cout << G[i].first.first << " " << G[i].first.second << " " << G[i].second; cout << endl;}cout << endl;
-  
-  vector< pair< pair<int, int>, int > > T = G;
-  //~ T.erase(T.begin()+3);
-  //~ T.erase(T.begin()+7);
-  //~ T.erase(T.begin()+3);
-  //~ for(int i=0; i<(int)T.size(); i++) {cout << T[i].first.first << " " << T[i].first.second << " " << T[i].second; cout << endl;}
-   
-  return T;
+vector< vector<int> > matrizDeAdyacencia(vector< pair< pair<int, int>, int > > &G, int n) {
+  vector <vector <int> > res(n+1, std::vector<int>(n+1));
+  for(int i=0; i<(int)G.size(); i++) {
+      res[G[i].first.first][G[i].first.second] = G[i].second; 	 
+      res[G[i].first.second][G[i].first.first] = G[i].second; 	    
+  }
+  int INF = MAX_INT;
+  for(int i=1; i<=n; i++) {
+	  for(int j=1; j<=n; j++) {
+		  if(res[i][j] == 0) {res[i][j] = INF;}
+	  }
+  }
+  return res;	
 }
 
 vector<nodo> generar_grafo_nodos(vector< pair< pair<int, int>, int > > &G, int n) {
@@ -119,17 +121,17 @@ vector<nodo> generar_grafo_nodos(vector< pair< pair<int, int>, int > > &G, int n
     G2.push_back(v);
   }
   for(int i=0; i<(int)G.size(); i++) {
-    G2[G[i].first.first].adyacentes.push_back(G[i].first.second);
-    G2[G[i].first.second].adyacentes.push_back(G[i].first.first);
+    G2[G[i].first.first].adyacentes.push_back(make_pair(G[i].first.second,G[i].second));
+    G2[G[i].first.second].adyacentes.push_back(make_pair(G[i].first.first,G[i].second));
   }
   return G2;
 }
 
 void dfs(nodo base, vector<int> &visitados, vector<nodo> &G2) {
   for(int i=0; i<(int)base.adyacentes.size(); i++) {
-      if(visitados[base.adyacentes[i]] == 0) {
-	      visitados[base.adyacentes[i]]++;
-	      dfs(G2[base.adyacentes[i]],visitados, G2);
+      if(visitados[base.adyacentes[i].first] == 0) {
+	      visitados[base.adyacentes[i].first]++;
+	      dfs(G2[base.adyacentes[i].first],visitados, G2);
 	  }
   }
 } 
@@ -156,7 +158,8 @@ bool sonIguales(pair<int, int> A, pair<int, int> B) {
 }
 
 void BuscarEjeParaCicloYAgregar(vector< pair< pair<int, int>, int > > &G,
-                                vector< pair< pair<int, int>, int > > &T) {
+                                vector< pair< pair<int, int>, int > > &T) {	// asumo G y T ordenados
+																			// asumo que G no es un árbol
     bool flag = false;
     int i = 1, j = 1;
     while(flag == false) {
@@ -177,13 +180,13 @@ nodo dfsCiclo(nodo base, vector<int> &visitados, vector<nodo> &G2, vector<int> &
   if(visitados[base.valor] == 2) { return base; }
   for(int i=0; i<(int)base.adyacentes.size(); i++) {
 	  
-	  if(padres[base.valor] == base.adyacentes[i]) {continue;}
+	  if(padres[base.valor] == base.adyacentes[i].first) {continue;}
 	  
 	  nodo temp;
-      padres[base.adyacentes[i]] = base.valor;
+      padres[base.adyacentes[i].first] = base.valor;
       //~ cout << base.valor << " es el padre de " << base.adyacentes[i] << endl;
-      if(visitados[base.adyacentes[i]] < 2) {
-	      temp = dfsCiclo(G2[base.adyacentes[i]],visitados, G2,padres);
+      if(visitados[base.adyacentes[i].first] < 2) {
+	      temp = dfsCiclo(G2[base.adyacentes[i].first],visitados, G2,padres);
 	      if(visitados[temp.valor]==2) {return temp;}
 	  }
   }
@@ -191,37 +194,64 @@ nodo dfsCiclo(nodo base, vector<int> &visitados, vector<nodo> &G2, vector<int> &
   return aux;
 } 
 
-vector<nodo> BuscarCiclo(vector< pair< pair<int, int>, int > > &T, int n) {
+pair< vector< pair< pair<int, int>, int > >, vector< pair< pair<int, int>, int > > > BuscarCicloYResto(vector< pair< pair<int, int>, int > > &T, int n) {
   
   vector<int> visitados(n+2,0);		// tam(T) = n+2: un arbol con un ciclo
   vector<int> padres(n+2,0);
-  vector<nodo> res;
+  vector<nodo> resTemp;				// guardo los nodos pertenecientes al ciclo
   vector<nodo> T2 = generar_grafo_nodos(T,n);
   
   nodo nodox = dfsCiclo(T2[1],visitados,T2,padres);
   
   //~ cout << "procesé bien al ciclo. Devuelvo el nodo: " << nodox.valor << endl;
-  for(int i=1; i<(int)padres.size(); i++) {cout << visitados[i] << " ";}cout << endl;
-  for(int i=1; i<(int)padres.size(); i++) {cout << padres[i] << " ";}cout << endl;
-  res.push_back(nodox);
+  //~ for(int i=1; i<(int)padres.size(); i++) {cout << visitados[i] << " ";}cout << endl;
+  //~ for(int i=1; i<(int)padres.size(); i++) {cout << padres[i] << " ";}cout << endl;
+  resTemp.push_back(nodox);
   int padre = padres[nodox.valor];
   while(padre != nodox.valor) {
-      res.push_back(T2[padre]);
+      resTemp.push_back(T2[padre]);
       padre = padres[T2[padre].valor];
   }
-  return res;
+  
+  vector< pair< pair<int, int>, int > > ciclo, resto;
+  vector<int> estan(n+1,0);						// registro qué elementos de T estan en el ciclo
+  for(int i=0; i<(int)resTemp.size(); i++) {
+	  
+	  int e1 = resTemp[i].valor;
+	  int e2 = resTemp[i+1].valor;
+      for(int j=1; j<(int)T.size(); j++) {
+	      if(T[j].first.first == e1 && T[j].first.second == e2 && padres[resTemp[i].valor] != nodox.valor) {estan[j] = 1;}
+	      if(T[j].first.first == e2 && T[j].first.second == e1 && padres[resTemp[i].valor] != nodox.valor) {estan[j] = 1;}
+	      
+	      if(padres[resTemp[i].valor] == nodox.valor && 
+	         ((T[j].first.first == nodox.valor && T[j].first.second == resTemp[i].valor) || 
+	          (T[j].first.first == resTemp[i].valor && T[j].first.second == nodox.valor))){
+			  ciclo.push_back(T[j]);	  
+		  }
+	  }
+  }
+  
+  for(int i=0; i<(int)T.size(); i++) {
+	  if(estan[i] == 1) {
+		  ciclo.push_back(T[i]);
+	  }else {
+	      resto.push_back(T[i]);
+	  }  
+  }
+  
+  return make_pair(ciclo,resto);
 }
 
-vector<nodo> BuscarResto(vector< pair< pair<int, int>, int > > &T,
-                         vector<nodo> &C) {
+
+void DevolverResultado(pair< vector< pair< pair<int, int>, int > >, vector< pair< pair<int, int>, int > > > &ANS) {
   
-  vector<nodo> R;
-  return R;
+  cout << "EL CICLO ES: " << endl;
+  for(int i=0; i<(int)ANS.first.size(); i++) {
+	  cout << ANS.first[i].first.first << " " << ANS.first[i].first.second << " " << ANS.first[i].second << endl;  
+  }
+  cout << "EL RESTO ES: " << endl;
+  for(int i=0; i<(int)ANS.second.size(); i++) {
+	  cout << ANS.second[i].first.first << " " << ANS.second[i].first.second << " " << ANS.second[i].second << endl;  
+  }
 }
 
-void DevolverResultado(vector< pair< pair<int, int>, int > > &T,
-                       vector< pair< pair<int, int>, int > > &C,
-                       vector< pair< pair<int, int>, int > > &R) {
-  
-  
-}
